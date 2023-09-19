e631b5c351522a14a11cc1f150e77d798c8e0b99
Windula Dissanayake <charithwinwindula@gmail.com>
2023-09-20 03:41:10 +0530

    commit

--- src/main/java/com/mycompany/repository/UserRepository.java
+++ src/main/java/com/mycompany/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.mycompany.repository;
 import com.mycompany.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User,Integer> {
+    List<User> findByFirstnameContainingIgnoreCase(String keyword);
 }
